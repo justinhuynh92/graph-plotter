4a989da7110a60ab6427fa626394bd56dfb88510
Justin Huynh <justinhuynh639@gmail.com>
2023-10-09 00:22:17 -0700

    finished graph plotter

--- app.py
+++ app.py
@@ -4,17 +4,16 @@ import matplotlib.pyplot as plt
 left = [1,2,3,4,5]
 height = [10,11,23,36,4]
 
-# plot the point
-plt.plot(x, y, color='green', linestyle='dashed', linewidth=3, marker='o', markerfacecolor='blue', markersize=12)
+tick_label = ['one', 'two', 'three', 'four', 'five']
 
-plt.ylim(1,8)
-plt.xlim(1,8)
+plt.bar(left, height, tick_label = tick_label, width = 0.8, color = ['blue', 'orange'])
+
 
 plt.xlabel('X Axis')
 
 plt.ylabel('Y Axis')
 
-plt.title('Demo Graph - Customization')
+plt.title('Demo Graph - Bar Chart')
 
 plt.legend
 
