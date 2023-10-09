26a22e571ee409225a11db291fefbb4dc9258385
Justin Huynh <justinhuynh639@gmail.com>
2023-10-09 00:19:42 -0700

    changed x and y axises

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import matplotlib.pyplot as plt
 
 # establish x and y axises
-x = [2, 4, 5 ,6]
-y = [2, 3, 6, 7]
+left = [1,2,3,4,5]
+height = [10,11,23,36,4]
 
 # plot the point
 plt.plot(x, y, color='green', linestyle='dashed', linewidth=3, marker='o', markerfacecolor='blue', markersize=12)
+
+plt.ylim(1,8)
+plt.xlim(1,8)
 
 plt.xlabel('X Axis')
 
 plt.ylabel('Y Axis')
 
-plt.title('Demo Graph - Two Lines')
+plt.title('Demo Graph - Customization')
 
 plt.legend
 
