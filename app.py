b728951c8607140fa674336f13d0579b09eef965
Justin Huynh <justinhuynh639@gmail.com>
2023-10-08 17:25:37 -0700

    started graph plotter

---
+++ app.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# establish x and y axises
+x = [2, 4, 5]
+y = [2, 3, 6]
+
+# plot the point
+plt.plot(x, y)
+
+plt.xlabel('X Axis')
+
+plt.ylabel('Y Axis')
+
+plt.title('Demo Graph')
